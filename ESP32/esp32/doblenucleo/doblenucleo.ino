fcfac6a5041eac69a4ec97745e5a5954b97cb2d3
joffre <joffre.hermosilla@gmail.com>
2025-01-06 19:09:10 -0500

    chore: doble nucleo esp32 s3

--- ESP32/esp32/doblenucleo/doblenucleo.ino
+++ ESP32/esp32/doblenucleo/doblenucleo.ino
@@ -1,12 +1,54 @@
+#include <Arduino.h>
 
+TaskHandle_t Task2, Task3;
+int cuenta = 0;
+
+void loop2(void *parameter){
+  for(;;){
+    Serial.println("\t\t\tEn núcleo -> " +  String(xPortGetCoreID()));
+    delay(100);
+    cuenta++;
+    Serial.println("desde loop 2 -> " + String(cuenta));
+  }
+  vTaskDelay(10);
+}
+
+void loop3(void *parameter){
+  for(;;){
+    Serial.println("\t\t\t Loop 3 🤯🤯🤯");
+    delay(2000);
+    cuenta++;
+    Serial.println("desde loop 3 -> " + String(cuenta));
+  }
+  vTaskDelay(10);
+}
 
 void setup() {
+
+  xTaskCreatePinnedToCore(
+    loop2,
+    "Task_2",
+    1000,
+    NULL,
+    1,
+    &Task2,
+    0);
+
+  xTaskCreatePinnedToCore(
+      loop3,
+      "Task_3",
+      1000,
+      NULL,
+      1,
+      &Task3,
+      0);
+
   Serial.begin(115200);
-  Serial.print("setup() running on core ");
-  Serial.println(xPortGetCoreID());
 }
 
 void loop() {
-  Serial.print("loop() running on core ");
-  Serial.println(xPortGetCoreID());
+  Serial.println("En núcleo -> " +  String(xPortGetCoreID()));
+  delay(1000);
+  cuenta++;
+  Serial.println("desde loop -> " + String(cuenta));
 }
--- sensortanque/arduinoleonardo/Knob-leonardo-servos/Knob-leonardo-servos.ino
+++ sensortanque/arduinoleonardo/Knob-leonardo-servos/Knob-leonardo-servos.ino
@@ -1,3 +1,7 @@
+#include <Servo.h>
+
+#include <Servo.h>
+
 /*
  Controlling a servo position using a potentiometer (variable resistor)
  by Michal Rinott <http://people.interaction-ivrea.it/m.rinott>
@@ -10,17 +14,35 @@
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
-
+Servo servoMotor; 
 int potpin = A0;  // analog pin used to connect the potentiometer
 int val;    // variable to read the value from the analog pin
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(9);  
+    servoMotor.attach(2);// attaches the servo on pin 2 to the servo object
 }
 
 void loop() {
+
+//COMUNICACION CON ESP8266
+Serial.println("Hello Boss I'm Arduino Leonardo");
+  delay(1500);
+
   val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
   val = map(val, 0, 150, 0, 180);     // scale it for use with the servo (value between 0 and 180)
   myservo.write(val);                  // sets the servo position according to the scaled value
-  delay(0.5);                           // waits for the servo to get there
+  delay(0.5);     
+  
+  // Desplazamos a la posición 0º
+  servoMotor.write(0);
+  delay(1000);  // Esperamos 1 segundo
+  
+  // Desplazamos a la posición 90º
+  servoMotor.write(90);
+  delay(1000);  // Esperamos 1 segundo
+  
+  // Desplazamos a la posición 180º
+  servoMotor.write(180);
+  delay(1000);  // Esperamos 1 segundo                      // waits for the servo to get there
 }
